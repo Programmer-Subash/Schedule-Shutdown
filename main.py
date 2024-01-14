8a7d2bf32bca325cd0d8d5f21d829b04aac257b8
Programmer-Subash <programmersubash33@gmail.com>
2024-01-14 16:33:53 +0545

    tkinter and subprocess modules are imported main window is created using tkinter

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+import subprocess
+
+# create window for application
+window = tk.Tk()
+window.title("Schedule Shutdown")
+window.minsize(width=800, height=600)
+
+
+
+# mainloop
+window.mainloop()

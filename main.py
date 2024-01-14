c758844d6b0b8840b67c6d4c001d8f6bbc4971f4
Programmer-Subash <programmersubash33@gmail.com>
2024-01-14 17:47:15 +0545

    version 1.0 subprocess to shutdown is created

--- main.py
+++ main.py
@@ -14,6 +14,19 @@ def convert_to_second(hour: int, minute: int):
     return hour * 60 * 60 + minute * 60
 
 
+# function that create subprocess
+def schedule_shutdown_subprocess():
+    hour = int(hour_slider.get())
+    minute = int(minute_slider.get())
+    second = convert_to_second(hour, minute)
+    try:
+        mess = subprocess.run(["powershell", "-command", "shutdown", "-s", "-t", f"{second}"], check=True)
+        status_label["text"] = "Successful"
+    except Exception as e:
+        status_label["text"] = "Failed"
+        print(e)
+
+
 # create hour label
 tk.Label(window, text="Hour", font=("Arian", 15), padx=20).grid(row=0, column=0)
 
@@ -29,8 +42,11 @@ minute_slider = tk.Scale(window, from_=0, to=60, orient=tk.HORIZONTAL)
 minute_slider.grid(row=1, column=2)
 
 # create schedule shutdown button
-shutdown_button = tk.Button(window, text="Shutdown")
+shutdown_button = tk.Button(window, text="Shutdown", command=schedule_shutdown_subprocess)
 shutdown_button.grid(row=2, column=1)
 
+# create status label
+status_label = tk.Label(window, text="")
+status_label.grid(row=3, column=1)
 # mainloop
 window.mainloop()

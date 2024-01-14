0ee3589ac41c7347446faaddb82d39f001761777
Programmer-Subash <programmersubash33@gmail.com>
2024-01-14 16:52:52 +0545

    layout is created 2 label, 2 scale and 1 button
    a function that convert time to second is create
    convert_to_second

--- main.py
+++ main.py
@@ -4,9 +4,33 @@ import subprocess
 # create window for application
 window = tk.Tk()
 window.title("Schedule Shutdown")
-window.minsize(width=800, height=600)
+window.minsize(width=400, height=400)
+window.config(padx=50, pady=50)
+
+# function that convert time to second
 
 
+def convert_to_second(hour: int, minute: int):
+    return hour * 60 * 60 + minute * 60
+
+
+# create hour label
+tk.Label(window, text="Hour", font=("Arian", 15), padx=20).grid(row=0, column=0)
+
+# create hour slider
+hour_slider = tk.Scale(window, from_=0, to=24, orient=tk.HORIZONTAL)
+hour_slider.grid(row=0, column=2)
+
+# create minute label
+tk.Label(window, text="Minute", font=("Arian", 15), padx=20).grid(row=1, column=0)
+
+# create minute slider
+minute_slider = tk.Scale(window, from_=0, to=60, orient=tk.HORIZONTAL)
+minute_slider.grid(row=1, column=2)
+
+# create schedule shutdown button
+shutdown_button = tk.Button(window, text="Shutdown")
+shutdown_button.grid(row=2, column=1)
 
 # mainloop
 window.mainloop()
